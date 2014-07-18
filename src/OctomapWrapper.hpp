758f784eee6130e6b857b39f97ac7d9c6151e966
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2014-07-18 21:37:04 +0200

    cleanup header

--- src/OctomapWrapper.hpp
+++ src/OctomapWrapper.hpp
@@ -13,29 +13,25 @@ namespace octomap_wrapper {
 class OctomapWrapper {
 public:
 
- OctomapWrapper(){
+        OctomapWrapper() {};
+        ~OctomapWrapper(){};
 
- };
+        // Timestamp for this octomap
+        //
+        // This is the timestamp of the data that got added last in the map
+        base::Time time;
 
- ~OctomapWrapper(){};
+        // Flag to denote a binary (only free/occupied) or full occupancy octree (.bt/.ot file)
+        bool binary;
 
- 	//Origin (in m) of the map
+        // Class id of the contained octree
+        std::string id;
 
-    // binary serialization of octree, use Conversion.hpp to read and write octrees
-    std::vector<boost::int8_t> data;
+        // Resolution (in m) of the smallest octree nodes
+        double resolution;
 
- 	//timestamp of the sonar beam
- 	//base::Time time;
-
-	// Flag to denote a binary (only free/occupied) or full occupancy octree (.bt/.ot file)
-	bool binary;
-
-	// Class id of the contained octree
-	std::string id;
-
-	//Resolution (in m) of the smallest octree nodes
-	double resolution;
-
+        // binary serialization of octree, use Conversion.hpp to read and write octrees
+        std::vector<boost::int8_t> data;
 };
 
 }
